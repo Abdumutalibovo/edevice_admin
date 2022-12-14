ab5379baab19f85a03655986546588169394c3cd
Abdumutalibovo <odilbekjuraev07@gmail.com>
2022-12-14 16:12:11 +0500

    all device notification done 🤭

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,21 @@ import 'package:edevice_admin/view_model/profile_view_model.dart';
 import 'package:edevice_admin/view_model/tab_view_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'data/repositories/auth_repository.dart';
 import 'data/repositories/categories_repository.dart';
 import 'data/repositories/product_repository.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async{
+  // await Firebase.initializeApp();
+  print("Handling a background message : ${message.messageId}");
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await Firebase.initializeApp();
   var fireStore = FirebaseFirestore.instance;
 
--- lib/presentation/admin/admin_screen.dart
+++ lib/presentation/admin/admin_screen.dart
@@ -1,15 +1,69 @@
 import 'package:edevice_admin/presentation/admin/products/all_products_screen.dart';
+import 'package:edevice_admin/presentation/chat/chat_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'category/all_category_screen.dart';
 
-class AdminScreen extends StatelessWidget {
+class AdminScreen extends StatefulWidget {
   const AdminScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  _printFirebaseCloudMessagingToken() async {
+    String? token = await FirebaseMessaging.instance.getToken();
+    print("FCM TOKEN : $token");
+  }
+
+  _handleFirebaseNotificationMessage() async {
+    //Forgrounddan kelgan messagelarni tutib olamiz!
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("Foregroundda message ushladim");
+      print("Message data : ${message.data}");
+
+      if (message.notification != null) {
+        print("NATIFICATION BOR: ${message.notification}");
+        print(message.notification!.title);
+        print(message.notification!.body);
+      }
+    });
+  }
+
+  _setupInteractedMessage() async {
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+    //Terminateddan kirganda bu ishlaydi
+    if (initialMessage != null) {
+      _handleMessage(initialMessage);
+    }
+    //Backgrounddan kirganda  shu ishlaydi
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  }
+  _handleMessage(RemoteMessage message) {
+    if (message.data['type'] == 'chat') {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ChatScreen(),
+          ));
+    }
+  }
+
+  @override
+  void initState() {
+    _printFirebaseCloudMessagingToken();
+    _handleFirebaseNotificationMessage();
+    _setupInteractedMessage();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Admin Page",
           style: TextStyle(color: Colors.black),
         ),
@@ -26,7 +80,7 @@ class AdminScreen extends StatelessWidget {
                 MaterialPageRoute(builder: (context) => AllCategoriesScreen()),
               );
             },
-            title: Text("Categories"),
+            title: const Text("Categories"),
           ),
           ListTile(
             onTap: () {
